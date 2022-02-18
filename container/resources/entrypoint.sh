71bf2bc6e6a572afd1c6f6bb6d657b7748c2cc5c
Eugene Williams <eugene@grplife.com>
2022-02-18 11:21:57 -0800

    updates

--- container/resources/entrypoint.sh
+++ container/resources/entrypoint.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 #echo "Will load log4j file using region: ${region}"
-/usr/lib/postgresql/9.3/bin/postgres -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf
-psql -U docker -P docker
+#/usr/lib/postgresql/9.3/bin/postgres -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf
+#psql -U docker -P docker
 export JAVA_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=1043"
-exec java $JAVA_OPTS -jar qamanager.jar
+exec java $JAVA_OPTS -jar -Dspring.profiles.active=prod qamanager.jar
